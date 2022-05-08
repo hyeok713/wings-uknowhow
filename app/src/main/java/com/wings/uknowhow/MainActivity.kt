bbed5e8748cfd061c4fb043fb526ca2a77946d3a
HYEOKBEOM <part1shb@gmail.com>
2022-05-08 15:12:58 +0900

    Compose list of text like a chat room.

--- app/src/main/java/com/wings/uknowhow/MainActivity.kt
+++ app/src/main/java/com/wings/uknowhow/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -30,11 +35,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MessageCard(Message("Hyeok", "Jetpack Compose"))
+                    Conversation(getSampleData().conversationSample)
+//                    MessageCard(Message("Hyeok", "Jetpack Compose"))
                 }
             }
         }
     }
+
+
+}
+
+data class SampleData(val conversationSample: List<Message>)
+
+fun getSampleData(): SampleData {
+    return SampleData(
+        arrayListOf<Message>(
+            Message("Hyeok", "Hello guys"),
+            Message("Hye", "Hello guys"),
+            Message(
+                "ok",
+                "this is my first compose project.\nthis is my first compose project.\nthis is my first compose project." +
+                        "\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project." +
+                        "\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project." +
+                        "\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project."
+            ),
+            Message("eok", "guys"),
+            Message("ffffk", "test"),
+            Message("powerovjer", "hi")
+        )
+    )
 }
 
 
@@ -55,9 +90,18 @@ fun MessageCard(msg: Message) {
 
         Spacer(Modifier.width(8.dp))        // Horizontal Space
 
+        // variable to remember states
+        var isExpanded: Boolean by remember { mutableStateOf(false) }
+        // surfaceColor by remember state
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
         // Column -> Vertical
         Column {
             Text(
+                modifier = Modifier.clickable {
+                   println(msg.author)
+                },
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle1
@@ -66,26 +110,65 @@ fun MessageCard(msg: Message) {
             Spacer(Modifier.height(4.dp))   // Vertical Space
 
             // Surface is such like a RelativeLayout(size of wrap_contents)
-            Surface(shape = MaterialTheme.shapes.small, elevation = 1.dp) {
+            Surface(
+                modifier = Modifier.clickable {
+                    isExpanded = !isExpanded    // Toggle a state of expanded
+                }.also {
+                    Modifier
+                        .animateContentSize()
+                        .padding(1.dp)
+                },
+                shape = MaterialTheme.shapes.small,
+                elevation = 1.dp,
+                color = surfaceColor
+            ) {
                 Text(
                     text = msg.body,
                     modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
+                    style = MaterialTheme.typography.body2,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 2,
                 )
             }
         }
     }
 }
 
-@Preview(name = "Light Mode")
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+
+@Preview(name = "Light Mode", showBackground = true)
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = false, name = "Dark Mode")
 @Composable
 fun DefaultPreview() {
+    data class SampleData(val conversationSample: List<Message>)
+
+    val sampleData = SampleData(
+        arrayListOf<Message>(
+            Message("Hyeok", "Hello guys"),
+            Message("Hye", "Hello guys"),
+            Message(
+                "ok",
+                "this is my first compose project.\nthis is my first compose project.\nthis is my first compose project.\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project.\n" +
+                        "this is my first compose project."
+            ),
+            Message("eok", "guys"),
+            Message("ffffk", "test"),
+            Message("powerovjer", "hi")
+        )
+    )
+
     UknowhowTheme {
-        MessageCard(Message("Hyeok's", "Jetpack Compose"))
+//        MessageCard(Message("Hyeok's", "Jetpack Compose"))
+        Conversation(messages = sampleData.conversationSample)
     }
 }
